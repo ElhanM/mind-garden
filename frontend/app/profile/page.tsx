c296927960fef4cc5aded7dacfc278c88285b723
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-03-03 14:12:27 +0100

    Fixes #5: User data inside of profile page (#9)
    * Fixes #5: User data displayed instead of the previously generated form
    
    * Fix: Image attribute switched to 'required' to ensure its presence
    
    * Images in config updated to accept 'googleusercontent' sources
    
    * Skeleton loading added for user information

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,42 +1,55 @@
+'use client';
 import { CardWithTitle } from '@/components/ui/card-with-title';
 import { PageLayout } from '@/components/page-layout';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { StreaksCalendar } from '@/components/streaks-calendar';
 import { MoodGraph } from '@/components/mood-graph';
+import { useSession } from 'next-auth/react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function ProfilePage() {
+  const { data: session, status } = useSession();
+  const isLoading = status === 'loading';
+
   return (
     <PageLayout>
-      <div className="grid gap-6 md:grid-cols-2">
-        <CardWithTitle title="Personal Information">
-          <div className="mb-6 flex justify-center">
-            <Image
-              src="/placeholder.svg"
-              alt="Profile Picture"
-              width={150}
-              height={150}
-              className="rounded-full"
-            />
-          </div>
-          <form className="space-y-4">
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input type="text" id="name" placeholder="Your Name" />
+      <div className="grid gap-6 md:grid-cols-2 items-stretch">
+        <div className="h-fit bg-white p-6 rounded-lg shadow">
+          <CardWithTitle title="Personal Information">
+            <div className="mb-6 flex justify-center">
+              {isLoading ? (
+                <Skeleton className="h-[145px] w-[145px] rounded-full" />
+              ) : (
+                <Image
+                  src={session?.user.image || '/Logo.png'}
+                  alt="Profile Picture"
+                  width={145}
+                  height={145}
+                  className="rounded-full"
+                />
+              )}
             </div>
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input type="email" id="email" placeholder="your.email@example.com" />
+            <hr />
+            <div className="space-y-4 mt-2">
+              <div className="grid w-full items-center gap-1.5">
+                <span className="font-bold">User Name:</span>
+                {isLoading ? (
+                  <Skeleton className="h-6 w-[200px]" />
+                ) : (
+                  <span>{session?.user.name}</span>
+                )}
+              </div>
+              <div className="grid w-full items-center gap-1.5">
+                <span className="font-bold">E-mail:</span>
+                {isLoading ? (
+                  <Skeleton className="h-6 w-[250px]" />
+                ) : (
+                  <span>{session?.user.email}</span>
+                )}
+              </div>
             </div>
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="birthdate">Birthdate</Label>
-              <Input type="date" id="birthdate" />
-            </div>
-            <Button type="submit">Update Profile</Button>
-          </form>
-        </CardWithTitle>
+          </CardWithTitle>
+        </div>
         <div className="space-y-6">
           <CardWithTitle title="Your Streaks">
             <StreaksCalendar />
