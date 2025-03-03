2cc57a9e24683f35bede7fb9cf61eb51ccda03b0
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2025-03-03 13:49:21 +0100

    Fixes #5: User data displayed instead of the previously generated form

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,42 +1,40 @@
+'use client';
 import { CardWithTitle } from '@/components/ui/card-with-title';
 import { PageLayout } from '@/components/page-layout';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { StreaksCalendar } from '@/components/streaks-calendar';
 import { MoodGraph } from '@/components/mood-graph';
+import { useSession } from 'next-auth/react';
 
 export default function ProfilePage() {
+  const { data: session } = useSession();
   return (
     <PageLayout>
-      <div className="grid gap-6 md:grid-cols-2">
-        <CardWithTitle title="Personal Information">
-          <div className="mb-6 flex justify-center">
-            <Image
-              src="/placeholder.svg"
-              alt="Profile Picture"
-              width={150}
-              height={150}
-              className="rounded-full"
-            />
-          </div>
-          <form className="space-y-4">
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input type="text" id="name" placeholder="Your Name" />
+      <div className="grid gap-6 md:grid-cols-2 items-stretch">
+        <div className="h-fit bg-white p-6 rounded-lg shadow">
+          <CardWithTitle title="Personal Information">
+            <div className="mb-6 flex justify-center">
+              <Image
+                src={session?.user.image ?? '/Logo.png'}
+                alt="Profile Picture"
+                width={145}
+                height={145}
+                className="rounded-full"
+              />
             </div>
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input type="email" id="email" placeholder="your.email@example.com" />
+            <hr />
+            <div className="space-y-4 mt-2">
+              <div className="grid w-full items-center gap-1.5">
+                <span className="font-bold">User Name:</span>
+                {session?.user.name}
+              </div>
+              <div className="grid w-full items-center gap-1.5">
+                <span className="font-bold">E-mail:</span>
+                {session?.user.email}
+              </div>
             </div>
-            <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="birthdate">Birthdate</Label>
-              <Input type="date" id="birthdate" />
-            </div>
-            <Button type="submit">Update Profile</Button>
-          </form>
-        </CardWithTitle>
+          </CardWithTitle>
+        </div>
         <div className="space-y-6">
           <CardWithTitle title="Your Streaks">
             <StreaksCalendar />
