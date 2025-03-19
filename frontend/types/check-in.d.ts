451727551e9cfe3d9218bcabdd8739cf74ec3c39
Mustafa Sinanovic <mustafasinanovic@outlook.com>
2025-03-19 01:50:28 +0100

    Added column checkInDate as in db

--- frontend/types/check-in.d.ts
+++ frontend/types/check-in.d.ts
@@ -1,6 +1,7 @@
 type CheckIn = {
   id: number;
-  createdAt: string; // Or `Date` if already parsed
+  createdAt: string; // Or `Date` if already parsed,
+  checkInDate: string;
   mood: 'great' | 'good' | 'okay' | 'down' | 'bad';
   stressLevel: number;
   journalEntry?: string;
