a9932f14193d44f0e86da04bc5f31680479bae0a
Mustafa Sinanovic <mustafasinanovic@outlook.com>
2025-04-01 22:38:38 +0200

    Removed CheckIn Type

--- frontend/types/check-in.d.ts
+++
@@ -1,8 +0,0 @@
-type CheckIn = {
-  id: number;
-  createdAt: string; // Or `Date` if already parsed,
-  checkInDate: string;
-  mood: 'great' | 'good' | 'okay' | 'down' | 'bad';
-  stressLevel: number;
-  journalEntry?: string;
-};
