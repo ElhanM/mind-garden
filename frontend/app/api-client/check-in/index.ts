c9a76f7629dffc790eb5d8885c225e784d204102
Vedad Kruho <vedo.kruhohz@gmail.com>
2025-04-04 22:59:48 +0200

    WP fetch ne funkcionise

--- frontend/app/api-client/check-in/index.ts
+++ frontend/app/api-client/check-in/index.ts
@@ -5,9 +5,7 @@ import type { CheckInFormData } from '../../../validation/check-in-schema';
 export const fetchTodayCheckIn = async (email: string | null) => {
   if (!email) return null;
 
-  const response = await api.get(`/api/check-ins`, {
-    headers: { 'user-email': email },
-  });
+  const response = await api.get(`/api/check-ins`);
 
   if (response.data.success !== true) {
     throw new Error(response.data.message);
@@ -17,13 +15,7 @@ export const fetchTodayCheckIn = async (email: string | null) => {
 };
 
 export const submitCheckIn = async (data: CheckInFormData, email: string) => {
-  const response = await api.post(
-    `/api/check-ins`,
-    { ...data },
-    {
-      headers: { 'user-email': email },
-    }
-  );
+  const response = await api.post(`/api/check-ins`, { ...data });
 
   if (response.data.success !== true) {
     throw new Error(response.data.message);
@@ -31,3 +23,15 @@ export const submitCheckIn = async (data: CheckInFormData, email: string) => {
 
   return response.data.results;
 };
+
+export async function fetchCheckInsHistory(email: string) {
+  const response = await api.get(`/api/check-ins/history`, {
+    method: 'GET',
+  });
+
+  if (response.data.success !== true) {
+    throw new Error(response.data.message);
+  }
+
+  return response.data.results;
+}
