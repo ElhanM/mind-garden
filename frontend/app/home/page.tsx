7748df1b7177f2257625d625acf5832238f1f5d3
Elhan Mahmutovic <elhan.mahmutovic03@gmail.com>
2025-05-24 10:28:36 +0200

    fix hydration error

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -207,7 +207,7 @@ export default function Home() {
                 ].map((item, index) => (
                   <div key={index} className={`rounded-lg ${item.bgColor} p-4 text-center`}>
                     <p className={`text-sm ${item.textColor}`}>{item.label}</p>
-                    <p className={`text-3xl font-bold ${item.textColor}`}>{item.value}</p>
+                    <div className={`text-3xl font-bold ${item.textColor}`}>{item.value}</div>
                   </div>
                 ))}
               </div>
